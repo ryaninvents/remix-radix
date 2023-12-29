e2f5cd5d26912ee95abcc9559b2515a17f8d0573
Ryan Kennedy <ryan@rmkennedy.com>
2023-12-29 03:07:21 +0000

    naive attempt to add Radix

--- www/app/root.jsx
+++ www/app/root.jsx
@@ -1,27 +1,25 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-} from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts } from '@remix-run/react';
+import * as Radix from '@radix-ui/themes';
+import radixStylesheet from '@radix-ui/themes/styles.css';
+
+export const links = () => [{ rel: 'stylesheet', href: radixStylesheet }];
 
 export default function App() {
   return (
-    <html>
-      <head>
-        <link
-          rel="icon"
-          href="data:image/x-icon;base64,AA"
-        />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <h1>Hello world!</h1>
-        <Outlet />
+    <Radix.Theme>
+      <html>
+        <head>
+          <link rel="icon" href="data:image/x-icon;base64,AA" />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <h1>Hello world!</h1>
+          <Outlet />
 
-        <Scripts />
-      </body>
-    </html>
+          <Scripts />
+        </body>
+      </html>
+    </Radix.Theme>
   );
 }
